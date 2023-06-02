a44e1d825d142b221a17e0f2cf092f8fc37508bb
Romulogome <romulo23596@hotmail.com>
2023-06-01 22:05:57 -0300

    Prova Teste

---
+++ Classes/Conta.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import datetime as dt
+
+class Conta:
+    def __init__(self, n_conta:str="",dataleitura:datetime=datetime.now(),nleitura:str="",valor:float=0,kwgasto:int=0)->None:
+        self.__n_conta = n_conta 
+        self.__dataleitura = dataleitura
+        self.__nleitura = nleitura
+        self.__valor_pagar = valor
+        self.__data_pgto = dt.timedelta(days=20)+dataleitura
+        self.__kw_gasto = kwgasto 
+
+    def getDataLeitura(self):
+        return self.__dataleitura.strftime(" %d %m %Y ")
+    def getKwGasto(self):
+        return self.__kw_gasto
+    def getValorPagar(self): 
+        return self.__valor_pagar
+    def getDadosConta(self):
+        pass
